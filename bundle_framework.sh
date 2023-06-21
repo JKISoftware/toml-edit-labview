907aea7c1750940351f5b53169c7e20d160fb6c0
Jim Kring <jim.kring@gmail.com>
2023-06-20 22:09:48 -0700

    don't build inside bundle framework script

--- bundle_framework.sh
+++ bundle_framework.sh
@@ -4,8 +4,8 @@ BUILD_TYPE=release
 SHARED_LIBARY_NAME=libtoml.dylib
 BUILD_OUTPUT_DIR=target/$BUILD_TYPE
 
-# build shared library with Cargo
-cargo build --$BUILD_TYPE
+# # build shared library with Cargo
+# cargo build --$BUILD_TYPE
 
 # Create a variable with the name of the framework
 FRAMEWORK_NAME=dragon_toml_64
